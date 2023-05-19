53880e946961ebafc5c0df0d3ac304d63a95ba89
Mahfuz-Hasan <mahfuzhasan587@gmail.com>
2023-05-20 00:56:51 +0600

    firebase configaration

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,19 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBb0N725RzJ4Z9NzAsvYuNmubyQdAjl1Js",
-  authDomain: "hero-verse-client.firebaseapp.com",
-  projectId: "hero-verse-client",
-  storageBucket: "hero-verse-client.appspot.com",
-  messagingSenderId: "950736692878",
-  appId: "1:950736692878:web:79ae04d827918747098bdf",
-  measurementId: "G-GLME7QEBBB"
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
