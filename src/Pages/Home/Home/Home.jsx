d829f952a6f5a7ae1cb141f6b9c60f3baa94f196
Mahfuz-Hasan <mahfuzhasan587@gmail.com>
2023-05-21 20:42:47 +0600

    bonus part-1 added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Banner from './Banner/Banner';
 import ImageGallary from './ImageGallary/ImageGallary';
 import ReactTabs from './ReactTabs/ReactTabs';
+import LocalTIme from './LocalTIme/LocalTIme';
 
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <LocalTIme></LocalTIme>
             <ImageGallary></ImageGallary>
             <ReactTabs></ReactTabs>
         </div>
---
+++ src/Pages/Home/Home/LocalTIme/LocalTIme.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import { FaClock } from "react-icons/fa";
+const LocalTIme = () => {
+  let time = new Date().toLocaleTimeString();
+  const [currtime, setCurrtime] = useState(time);
+  const updateTime = () => {
+    let time = new Date().toLocaleTimeString();
+    setCurrtime(time);
+  };
+  setInterval(updateTime, 1000);
+  return (
+    <div className=" text-3xl lg:text-7xl flex items-center justify-center lg:gap-10 gap-5  w-fit mx-auto px-6 py-4 rounded-2xl button font-semibold my-20">
+     <FaClock></FaClock>
+      <div className="">{currtime}</div>
+    </div>
+  );
+};
+
+export default LocalTIme;
