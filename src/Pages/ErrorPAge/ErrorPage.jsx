198295dcceea93424b0f05de77a9fd9e940a1875
Mahfuz-Hasan <mahfuzhasan587@gmail.com>
2023-05-21 23:09:03 +0600

    back to homepage done

--- src/Pages/ErrorPAge/ErrorPage.jsx
+++ src/Pages/ErrorPAge/ErrorPage.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import error from '../../assets/error/error.jpeg'
+import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
     return (
         <div>
+            <div>
+                <Link to="/">
+                <button className='bg-yellow-300 p-3 rounded-lg font-bold'>Back to Homepage</button>
+                </Link>
+            </div>
             <img className='h-screen w-screen' src={error} alt="" />
         </div>
     );
